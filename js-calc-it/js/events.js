79439c61911ad5f88d02f5ad1a403c9b128aba10
Guillaume Perrin <gperrin01@gmail.com>
2015-05-05 18:50:50 +0100

    playing with bucket variable

--- js-calc-it/js/events.js
+++ js-calc-it/js/events.js
@@ -1,30 +1,31 @@
 // n1 then operator then n2
 // clicking number again will override
 
-var num1=''; var num2; var mathOp;
+var num1=''; var bucket; var mathOp; var eq;
 // do i need them as global or loca var??
 
-// create event listerner for numbers
+// create event listerner for numbers + concat numbers prssed in case we wanna do "75"
 var allNum = document.getElementsByClassName('number');
 for (var i=0; i<allNum.length; i++) {
   allNum[i].addEventListener('click', function (event) {
-    num1 = num1+this.value;
-    // concat numbers prssed in case we wanna do "75"
-    // return OR num1 = value OR local var ??
+    num1 += this.value;
   })
 }
 
-// create event listerner for operator
+// create event listerner for operator, store num1 value into a bucket and reset value of num1
 var allOps = document.getElementsByClassName('operator');
 for (var i=0; i<allOps.length; i++) {
   allOps[i].addEventListener('click', function (event) {
+    bucket = num1;
     mathOp = this.value;
+    num1 = '';
   })
 }
 
-
 //create event listerner for equal
-
+document.getElementsByClassName('eq').addEventListener('click', function (event) {
+  eq = this.value;
+})
 
 
 // create event listerner for clear

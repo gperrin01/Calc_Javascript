1ab3963305894b54dfab49e6c5afa7c190548bc7
Guillaume Perrin <gperrin01@gmail.com>
2015-05-05 18:40:45 +0100

    adding EventsListeners

--- js-calc-it/js/events.js
+++ js-calc-it/js/events.js
@@ -1,20 +1,26 @@
-// create event listerner for class numbers
 // n1 then operator then n2
 // clicking number again will override
 
+var num1=''; var num2; var mathOp;
+// do i need them as global or loca var??
+
+// create event listerner for numbers
 var allNum = document.getElementsByClassName('number');
 for (var i=0; i<allNum.length; i++) {
-  allNum[i].getEventListener('onclick', function (event) {
-    
+  allNum[i].addEventListener('click', function (event) {
+    num1 = num1+this.value;
+    // concat numbers prssed in case we wanna do "75"
+    // return OR num1 = value OR local var ??
   })
 }
 
-
-
-
-// create event listerner for class operator
-
-
+// create event listerner for operator
+var allOps = document.getElementsByClassName('operator');
+for (var i=0; i<allOps.length; i++) {
+  allOps[i].addEventListener('click', function (event) {
+    mathOp = this.value;
+  })
+}
 
 
 //create event listerner for equal

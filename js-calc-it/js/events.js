8473137f35960d59d11008602bfe82bc91a17221
Guillaume Perrin <gperrin01@gmail.com>
2015-05-05 17:37:38 +0100

    initial JS file structure

---
+++ js-calc-it/js/events.js
@@ -0,0 +1,54 @@
+// create event listerner for class numbers
+// n1 then operator then n2
+// clicking number again will override
+
+var allNum = document.getElementsByClassName('number');
+for (var i=0; i<allNum.length; i++) {
+  allNum[i].getEventListener('onclick', function (event) {
+    
+  })
+}
+
+
+
+
+// create event listerner for class operator
+
+
+
+
+//create event listerner for equal
+
+
+
+// create event listerner for clear
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function add(x,y) {
+
+// }
+// function subtract(x,y) {
+
+// }
+// function divide(x,y) {
+  
+// }
+// function multiply(x,y) {
+
+// }
+// function clear() {
+
+// }
